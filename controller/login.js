1988b4cbfe47321cb9e98571bc218c1d31585bee
Rahil666 <rahilramesh666@gmail.com>
2025-08-05 16:24:03 +0530

    addbike,login

--- controller/login.js
+++ controller/login.js
@@ -1,33 +1,33 @@
-const model =require('../model/login');
-const bcrypt=require('bcrypt');
+const model = require('../model/login');
+const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 
 
-module.exports.login= async(req,res)=>{
-    try{
-    const{emailorphone_number,password,role}=req.body;
-    if(!emailorphone_number||!password||!role){
-        return res.status(400).json({
-                result:false,
-                message:'insufficient parameters',
+module.exports.login = async (req, res) => {
+  try {
+    const { emailorphone_number, password, role } = req.body;
+    if (!emailorphone_number || !password || !role) {
+      return res.status(400).json({
+        result: false,
+        message: 'insufficient parameters',
 
-              });
+      });
     }
-        const SECRET_KEY = 'dkjghkdghfhglknghdxlkdnflsfjopoijoigjhpokp';
+    const SECRET_KEY = 'dkjghkdghfhglknghdxlkdnflsfjopoijoigjhpokp';
 
-const checkUser = await model.CheckUser(emailorphone_number,role);
+    const checkUser = await model.CheckUser(emailorphone_number, role);
 
     console.log('checkUser:', checkUser);
-    
- 
+
+
     if (checkUser.length == 0) {
       return res.status(404).json({
         result: false,
         message: 'User not found',
       });
     }
-       // Verify password
+    // Verify password
     const isPasswordValid = await bcrypt.compare(password, checkUser[0].u_password);
     if (!isPasswordValid) {
       return res.status(401).json({
@@ -38,7 +38,7 @@ const checkUser = await model.CheckUser(emailorphone_number,role);
     const payload = {
       u_id: checkUser[0].u_id,
       email: checkUser[0].u_email,
-       u_role: checkUser[0].u_role
+      u_role: checkUser[0].u_role
 
     };
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
@@ -46,22 +46,22 @@ const checkUser = await model.CheckUser(emailorphone_number,role);
     return res.status(200).json({
       result: true,
       message: 'Login successful',
-      user:{
-         id: checkUser[0].u_id,
+      user: {
+        id: checkUser[0].u_id,
         name: checkUser[0].u_name,
-        email:checkUser[0].u_email,
+        email: checkUser[0].u_email,
         phone: checkUser[0].u_phone,
-        role:checkUser[0].u_role,
+        role: checkUser[0].u_role,
       }
     });
 
 
 
-    }catch (error) {
+  } catch (error) {
     console.error(error);
     return res.status(500).json({
       result: false,
       message: error.message,
-})
-    }
+    })
+  }
 }
