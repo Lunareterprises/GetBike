ae0ccc9fad464a8d4f744333fa5d39664aea0b44
amaldev8055 <adev86952@gmail.com>
2025-08-27 16:57:23 +0530

    center

--- controller/bikeceneter.js
+++ controller/bikeceneter.js
@@ -1,22 +1,22 @@
-var model=require('../model/bikecenter')
- 
+var model = require('../model/bikecenter')
 
 
 
-module.exports.addbikecenter =async(req,res)=>{
+
+module.exports.addbikecenter = async (req, res) => {
     try {
 
-        let{Center,district}=req.body;
+        let { Center, district } = req.body;
 
 
-        if(!Center||!district){
+        if (!Center || !district) {
             return res.send({
                 result: false,
                 message: "insufficent parameter"
             });
         }
-        let addbike=await model.AddBikeCenterQuery(Center,district);
-         if (addbike.affectedRows > 0) {
+        let addbike = await model.AddBikeCenterQuery(Center, district);
+        if (addbike.affectedRows > 0) {
             return res.send({
                 result: true,
                 message: "Bike center added successfully"
@@ -28,9 +28,9 @@ module.exports.addbikecenter =async(req,res)=>{
             });
         }
 
-        
+
     } catch (error) {
-       
+
         console.log(error);
         return res.send({
             result: false,
@@ -40,79 +40,79 @@ module.exports.addbikecenter =async(req,res)=>{
     }
 
 }
-module.exports.listcenter =async(req,res)=>{
-    try{
-        let{bc_id} = req.body;
+module.exports.listcenter = async (req, res) => {
+    try {
+        let { bc_id } = req.body;
 
-       var condition=""
-       if(bc_id){
-        condition=`where bc_id='${bc_id}'`
-       }
-
-
-     let listcenter = await model.listcenterQuery(condition);
-           if (listcenter.length > 0) {
-               return res.send({
-                   result: true,
-                   message: "data retrieved",
-                   list:  listcenter
-
-   
-   
-               });
-   
-           } else {
-               return res.send({
-                   result: false,
-                   messsage: "data not found",
-               });
-           }
-   
-   
-       } 
-       catch (error) {
-           return res.send({
-               result: false,
-               message: error.message,
-           });
-   
-       }
-    }
-    module.exports.deletecenter=async(req,res)=>{
-        try {
-            let bc_id=req.body.bc_id;
-
-            if(bc_id){
-                let checkcenter=await model.checkcenterQuery(bc_id);
-                if(checkcenter.length==0){
-                    return res.send({
-                        result:false,
-                        message:"Center details not found"
-                    })
-                }else{
-                    var deletesection=await model.removecenterQuery(bc_id);
-                    if(deletesection.affectedRows>0)
-                        return res.send({
-                    result:true,
-                    message:"center list deleted successfully"
-                })
-                }
-
-
-                }
-            
-            
-        } catch (error) {
-            return res.send({
-                result:false,
-                message:error.message,
-
-            })
-            
+        var condition = ""
+        if (bc_id) {
+            condition = `where bc_id='${bc_id}'`
         }
-        
+
+
+        let listcenter = await model.listcenterQuery(condition);
+        if (listcenter.length > 0) {
+            return res.send({
+                result: true,
+                message: "data retrieved",
+                list: listcenter
+
+
+
+            });
+
+        } else {
+            return res.send({
+                result: false,
+                messsage: "data not found",
+            });
+        }
+
+
     }
-    module.exports.editcenter = async (req, res) => {
+    catch (error) {
+        return res.send({
+            result: false,
+            message: error.message,
+        });
+
+    }
+}
+module.exports.deletecenter = async (req, res) => {
+    try {
+        let bc_id = req.body.bc_id;
+
+        if (bc_id) {
+            let checkcenter = await model.checkcenterQuery(bc_id);
+            if (checkcenter.length == 0) {
+                return res.send({
+                    result: false,
+                    message: "Center details not found"
+                })
+            } else {
+                var deletesection = await model.removecenterQuery(bc_id);
+                if (deletesection.affectedRows > 0)
+                    return res.send({
+                        result: true,
+                        message: "center list deleted successfully"
+                    })
+            }
+
+
+        }
+
+
+    } catch (error) {
+        return res.send({
+            result: false,
+            message: error.message,
+
+        })
+
+    }
+
+}
+module.exports.editcenter = async (req, res) => {
     try {
         let { bc_id, Center, district } = req.body;
 
